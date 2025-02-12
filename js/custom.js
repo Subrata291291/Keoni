5a01ab4c980e80fc974a3037da6eb24296451861
Subrata291291 <subrata.haldar@codeclouds.com>
2025-02-12 12:08:03 +0530

    Added Slider effects

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,14 @@ $(document).on('ready', function() {
       slidesToScroll: 1,
       arrows: false,
       fade: true,
+      infinite: true,
       asNavFor: '.center'
     });
     $('.center').slick({
       slidesToShow: 4,
       arrows: true,
       autoplay: true,
+      infinite: true,
       slidesToScroll: 1,
       asNavFor: '.regular',
       dots: false,
